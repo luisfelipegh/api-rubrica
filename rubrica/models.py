da0dc04883644722fc43f97c6ad6908a7b31f23c
Juanes Quintero <juaneschrome@gmail.com>
2019-09-04 12:32:24 -0500

    Initial commit

--- rubrica/models.py
+++ rubrica/models.py
@@ -13,15 +13,15 @@ def getSemester(now):
     year,month = now.year,now.month
     return str(year)+"2" if month>6 else str(year)+"1"
 
-class Usuario(models.Model):
-    username = models.CharField(max_length=20)  
-    cedula = models.IntegerField(max_length=11)
-    nombre = models.CharField(max_length=50)
+class Usuario(models.Model): 
+    cedula = models.IntegerField(default=0)
+    nombre = models.CharField(max_length=50,default='')
+    correo = models.EmailField(primary_key=True)
     def __str__(self):
         return self.username
 
 class Rubrica(models.Model):
-    nombre =  models.CharField(max_length=50)
+    nombre =  models.CharField(max_length=50,default='')
     semestre = models.IntegerField(default=getSemester(datetime.now())) 
     json = jsonfield.JSONField()
     usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)
