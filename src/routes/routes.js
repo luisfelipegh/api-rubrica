129858f2c19d59372b4bc6066a5210dde2881c3a
Luis Felipe Garcia Herrera <luisfelipegarciacsj@gmail.com>
2019-09-07 12:01:26 -0500

    cambios return usuarios

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = express.Router();
 routes.use(express.json());
 
+
 routes.use("/login", require("../controllers/login"));
 
 module.exports = routes;
